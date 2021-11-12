cbb3a8c4f70a1d3948c0345df2f230887c71b8de
emmanuel guzman <emmanuel.011593@gmail.com>
2021-11-12 00:15:17 -0400

    Clase math y llamdas de metodos

---
+++ conceptosPOO/ClasePunto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace conceptosPOO
+{
+    public class ClasePunto
+    {
+        public ClasePunto(int x, int y)
+        {
+            //Console.WriteLine($"la cordena x:{x} y la cordenada y:{y}");
+
+            this.x = x;
+            this.y = y;
+
+        }
+
+        public ClasePunto()
+        {
+            //Console.WriteLine($"la cordena no tienen valor en este contructor");
+
+            this.x = 0;
+            this.y = 0;
+
+        }
+
+        public double DitanciaHasta(ClasePunto punto)
+        {
+            int xDif = this.x - punto.x;
+            int yDif = this.y - punto.y;
+
+            double DitanciaPuntos = Math.Sqrt(Math.Pow(xDif,2)+Math.Pow(yDif,2));
+
+            return DitanciaPuntos;
+        }
+
+        private int x,y;
+
+    }
+}
---
+++ conceptosPOO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace conceptosPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            RealizarTarea();
+
+        }
+
+        static void RealizarTarea()
+        {
+            // TODO:APARTIR DE 
+            ClasePunto origen= new ClasePunto();
+            ClasePunto destino = new ClasePunto(128,80);
+
+            double distancia = origen.DitanciaHasta(destino);
+            Console.WriteLine(distancia);
+
+        }
+             
+        
+
+
+    }
+}

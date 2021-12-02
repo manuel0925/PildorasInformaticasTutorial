1fa81b22a847d1c590bc4bf1a95b080c11f4afe4
Emmanuel Guzman Encarnación <emmanuel.guzman@economia.gob.do>
2021-12-02 12:31:22 -0400

    herecia

--- UsoArrays/Program.cs
+++ UsoArrays/Program.cs
@@ -6,96 +6,28 @@ namespace UsoArrays
     {
         static void Main(string[] args)
         {
-            /*Console.WriteLine("Hello World!");
-
-            int[] edades;
-
-            edades =new int[4];
-
-            edades[0] = 5;
-            edades[2] = 6;
-            edades[2] = 7;
-            edades[3] = 8;
-
-            Console.WriteLine($"{edades[0]} {edades[1]} {edades[2]} {edades[3]}");
-
-
-            char[] letras = { 'a', 'b', 'c' };
-
-            Console.WriteLine(letras[1]);
-
-            var valoreRandom = new [] { "juan", "pedro"};
-
-            Console.WriteLine($"{valoreRandom[0]} {valoreRandom[1]}");
-
-            
-            */
-            Persona[] Personas = new Persona[2];
-
-            Persona maria = new Persona("maria", 32);
-            Persona juana = new Persona("juana", 25);
-
-            Personas[0] = juana;
-            Personas[1] = maria;
-
-            //Console.WriteLine($"{Personas[0].Edad} {Personas[1].Nombre}");
-
-            Animal[] Animales = new [] { new Animal("perro", 4), new Animal("pato", 2) };
-
-            //Console.WriteLine($"{Animales[0].Nombre} {Animales[0].Patas}");
-            //Console.WriteLine($"{Animales[1].Nombre} {Animales[1].Patas}");
-
-            //array de clase anonima o de tipo anonimo*/
-
-            var ninios = new[]
-            {
-                new {Nombre="bebo",Papa="eliza"},
-                new { Nombre = "zaid", Papa = "esteban" },
-                 new { Nombre = "moises", Papa = "gino" }
-            };
-
-            /*for (int i = 0; i < ninios.Length; i++)
-            {
-
-                Console.WriteLine(ninios[i]);
-
-            }*/
-
-            foreach (var ninio in ninios) {
-
-                Console.WriteLine(ninio.Nombre);
-            }
-
-
-
+            string[] Nombre = new string[] { "emmanuelk", "edwaerdo", "felix" };
+            ProcesarDatos();
         }
 
-       public class Persona {
-
-            public Persona(String Nombre,int Edad) {
-                this.Nombre = Nombre;
-
-                this.Edad = Edad;
-            }
-
-            public string Nombre;
-
-            public int Edad;
-        }
-
-        public class Animal
+        static int[] ProcesarDatos()
         {
 
-            public Animal(String Nombre, int Patas)
-            {
-                this.Nombre = Nombre;
+            Console.WriteLine("Cuanbto elemento quires que tenga el array");
+            string respuesta = Console.ReadLine();    
 
-                this.Patas = Patas;
+            int numeroElementos = int.Parse(respuesta);
+            
+            int[] elementos = new int[numeroElementos];
+
+            for (int i = 0; i < numeroElementos; i++)
+            {
+                elementos[i] = i + 1;
             }
 
-            public string Nombre;
-
-            public int Patas;
+            return elementos;
+           
         }
+
     }
 }
---
+++ herencia2/herencia2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace herencia2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Caballo Babieca = new Caballo("babieca");
+            Humanos Manuel = new Humanos("eliza");
+            Gorilla Copito = new Gorilla("copito");
+
+
+            Manuel.getNombre();
+
+
+        }
+    }
+
+    class Mamiferos
+    {
+        private string nombreServivo;
+        public Mamiferos(string nombre) 
+        {
+            nombreServivo = nombre;
+        }
+
+        public void respirar()
+        {
+            Console.WriteLine("soy capz de repisrar");
+        }
+
+        public void CuidarCrias()
+        {
+            Console.WriteLine("Cuido de mis crias hasta que se valgan pos is solas");
+        }
+
+        public void getNombre() { 
+            Console.WriteLine(nombreServivo);
+        }
+
+    }
+
+    class Caballo : Mamiferos
+    {   
+        public Caballo(string nombreCaballo): base(nombreCaballo)
+        {
+
+        }
+        public void galopar()
+        {
+            Console.WriteLine("soy capaz de galopar");
+        }
+    }
+
+    class Humanos : Mamiferos
+    {
+        public Humanos(string nombreHumano) : base(nombreHumano)
+        {
+
+        }
+        public void pensar()
+        {
+            Console.WriteLine("soy capaz de pensar");
+        }
+    }
+
+    class Gorilla : Mamiferos
+    {
+        public Gorilla(string nombreGorilla) : base(nombreGorilla)
+        {
+
+        }
+        public void trepar()
+        {
+            Console.WriteLine("soy capaz de trepar");
+        }
+    }
+
+}
